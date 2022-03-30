93e4c4bd45cf012496d99a364131e3635355be86
liaojiancai <v.ihhu@qq.com>
2022-03-30 14:34:13 +0800

    fix: 格式化文档

--- src/pages/Index/index.tsx
+++ src/pages/Index/index.tsx
@@ -5,9 +5,9 @@ import styles from './index.module.scss';
 
 const Index: FC = () => {
     return (
-      <div className="index-page">
+        <div className="index-page">
         index page
-      </div>
+        </div>
     );
 };
 
