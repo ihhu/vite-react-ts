714b32903975c61ab6d239e68f14f27426a976af
liaojiancai <v.ihhu@qq.com>
2022-04-02 10:06:10 +0800

    feat: 重命名

---
+++ src/pages/about/index.tsx
@@ -0,0 +1,8 @@
+import React, { FC } from 'react';
+import styles from './index.module.less';
+
+const About: FC = () => {
+    return <div className="about-page">about page</div>;
+};
+
+export default About;
---
+++ src/pages/index/index.tsx
@@ -0,0 +1,14 @@
+import React, { FC } from 'react';
+import { Button } from 'antd';
+import { LoadingOutlined } from '@ant-design/icons';
+import styles from './index.module.scss';
+
+const Index: FC = () => {
+    return (
+        <div className="index-page">
+        index page
+        </div>
+    );
+};
+
+export default Index;
