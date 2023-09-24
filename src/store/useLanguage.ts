c3a14efe1823dde180929f3bfa6070af3391f5f2
_ihhu <724415664@qq.com>
2023-09-24 11:04:43 +0800

    chore: file format

--- src/store/useLanguage.ts
+++ src/store/useLanguage.ts
@@ -1,36 +1,36 @@
 import { atom, useRecoilState } from 'recoil';
 
 enum KEYS {
-    localeList = 'language/localeList',
-    currentLocale = 'language/currentLocale',
+  localeList = 'language/localeList',
+  currentLocale = 'language/currentLocale',
 }
 interface LocaleInfo {
-    id: 'zh-CN' | 'en-US';
-    value: string;
+  id: 'zh-CN' | 'en-US';
+  value: string;
 }
 
 const language = {
-    localeList: atom<LocaleInfo[]>({
-        key: KEYS.localeList,
-        default: [
-            { id: 'zh-CN', value: '简体中文' },
-            { id: 'en-US', value: 'English' },
-        ],
-    }),
-    currentLocale: atom<LocaleInfo['id']>({
-        key: KEYS.currentLocale,
-        default: 'zh-CN',
-    }),
+  localeList: atom<LocaleInfo[]>({
+    key: KEYS.localeList,
+    default: [
+      { id: 'zh-CN', value: '简体中文' },
+      { id: 'en-US', value: 'English' },
+    ],
+  }),
+  currentLocale: atom<LocaleInfo['id']>({
+    key: KEYS.currentLocale,
+    default: 'zh-CN',
+  }),
 };
 
 import { setCurrentLocale } from '@/i18n';
 export default function useLanguage() {
-    const [currentLocale, setLocale] = useRecoilState(language.currentLocale);
-    return {
-        currentLocale,
-        updateLocale(locale: LocaleInfo['id']) {
-            setCurrentLocale(locale);
-            setLocale(locale);
-        },
-    };
+  const [currentLocale, setLocale] = useRecoilState(language.currentLocale);
+  return {
+    currentLocale,
+    updateLocale(locale: LocaleInfo['id']) {
+      setCurrentLocale(locale);
+      setLocale(locale);
+    },
+  };
 }
